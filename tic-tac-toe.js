58a29bf7c14476a141944fe031d61e507450569c
Richard Howlett <richardhowlett14@gmail.com>
2019-10-21 22:36:47 -0400

    Exercise 2 Complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,37 +4,74 @@
 window.onload = function() {
 	document.getElementsByTagName("button")[0].onclick = seeBoard;
 	
-	var gamestate=[0,0,0,0,0,0,0,0,0]; //Place holders for grid state
+	var gamestate=[1,1,1,1,1,1,1,1,1]; //Place holders for grid state
 	var turncount = 0; //Keeps track of the amount of turns, cannot be greater than 9
 	var state=0;
 	var board = document.getElementsByTagName("div");
 	var status = document.getElementById("status");
 
 	//Applies the square class to all divs contained in the board section of the html code
+	console.log(board.length);
 	function seeBoard(){
-		for (var i=3; i < board.length - 1; i++) {
+		for (var i=3; i <= 11; i++) {
 			board[i].setAttribute("class", "square");
 			board[i].innerHTML="";
-			board[i].setAttribute("onclick","makeMove("+i+")");
 		}
 	}
 
 	function makeMove(x){
 		if (board[x].className != "square X" && board[x].className != "square O"){
-			gamestate[x]=state;
+			gamestate[x-3]=state; // since the 1st square starts at the 3rd div 3 must be subtracted to be in the correct position
 			option(x);
 		}
-
+		else{
+			console.log("I didn't work");
+		}
+		console.log(gamestate);
 
 	}
 
-	function option (){
+	function option(x){
 		if (turncount<9){
-			if (board[x].classList.contains("hover")){
-				
+			if (state == 0){
+				board[x].setAttribute("class","square X");
+				board[x].innerHTML="X";
+				turncount++;
+				state=1
+			}
+			else{
+				board[x].setAttribute("class","square O");
+				board[x].innerHTML="O";
+				turncount++;
+				state=0
 			}
 		}
 	}
+
+	// Event Handlers for Boxes
+	board[3].onclick= function (){makeMove(3)};
+	board[4].onclick= function (){makeMove(4)};
+	board[5].onclick= function (){makeMove(5)};
+	board[6].onclick= function (){makeMove(6)};
+	board[7].onclick= function (){makeMove(7)};
+	board[8].onclick= function (){makeMove(8)};
+	board[9].onclick= function (){makeMove(9)};
+	board[10].onclick= function (){makeMove(10)};
+	board[11].onclick= function (){makeMove(11)};
+
+
+
+	//board[i].onmouseover = function(){hoverON(i-3)};
+			//board[i].onmouseout = function(){hoverOFF(i-3)};
+
+	/*
+	function hoverON(x){
+		board[x].classList.add("hover");
+	}
+
+	function hoverOFF(x){
+		board[x].classList.remove("hover");
+	} */
 
 
 }
