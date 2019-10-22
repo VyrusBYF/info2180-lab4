97fd6cc906dd868da7b78971150c92195e5dc572
Richard Howlett <richardhowlett14@gmail.com>
2019-10-21 23:49:40 -0400

    Finishing Up

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,12 @@
-//Lab 4 done by 620097204
+//Lab 4 done by 623397234
 //Tic-Tac-Toe Gam
 
 window.onload = function() {
 	document.getElementsByTagName("button")[0].onclick = seeBoard;
 	
-	var gamestate=[1,1,1,1,1,1,1,1,1]; //Place holders for grid state
+	var gamestate=[0,0,0,0,0,0,0,0,0]; //Place holders for grid state
 	var turncount = 0; //Keeps track of the amount of turns, cannot be greater than 9
-	var state=0;
+	var state=1;
 	var board = document.getElementsByTagName("div");
 	var status = document.getElementById("status");
 
@@ -17,11 +17,15 @@ window.onload = function() {
 			board[i].setAttribute("class", "square");
 			board[i].innerHTML="";
 		}
+		//Resets the values for a new game
+		state=1
+		gamestate=[0,0,0,0,0,0,0,0,0]
+		turncount = 1;
 	}
 
 	function makeMove(x){
 		if (board[x].className != "square X" && board[x].className != "square O"){
-			gamestate[x-3]=state; // since the 1st square starts at the 3rd div 3 must be subtracted to be in the correct position
+			gamestate[x-3]=state; // since the 4st square starts at the 3rd div 3 must be subtracted to be in the correct position
 			option(x);
 		}
 		else{
@@ -33,17 +37,25 @@ window.onload = function() {
 
 	function option(x){
 		if (turncount<9){
-			if (state == 0){
+			if (state == 1){
 				board[x].setAttribute("class","square X");
 				board[x].innerHTML="X";
 				turncount++;
-				state=1
+				state=-1
+				if (turncount>=5){
+					winner();
+				}
+				
 			}
 			else{
 				board[x].setAttribute("class","square O");
 				board[x].innerHTML="O";
 				turncount++;
-				state=0
+				state=1
+				if (turncount>=5){
+					winner();
+				}
+				
 			}
 		}
 	}
@@ -59,19 +71,118 @@ window.onload = function() {
 	board[10].onclick= function (){makeMove(10)};
 	board[11].onclick= function (){makeMove(11)};
 
+	//Event Handlers for Cursor Presence
+	board[3].onmouseover= function (){hoverON(3)};
+	board[4].onmouseover= function (){hoverON(4)};
+	board[5].onmouseover= function (){hoverON(5)};
+	board[6].onmouseover= function (){hoverON(6)};
+	board[7].onmouseover= function (){hoverON(7)};
+	board[8].onmouseover= function (){hoverON(8)};
+	board[9].onmouseover= function (){hoverON(9)};
+	board[10].onmouseover= function (){hoverON(10)};
+	board[11].onmouseover= function (){hoverON(11)};
+
+	//Event Handlers for Cursor Absence
+	board[3].onmouseout= function (){hoverOFF(3)};
+	board[4].onmouseout= function (){hoverOFF(4)};
+	board[5].onmouseout= function (){hoverOFF(5)};
+	board[6].onmouseout= function (){hoverOFF(6)};
+	board[7].onmouseout= function (){hoverOFF(7)};
+	board[8].onmouseout= function (){hoverOFF(8)};
+	board[9].onmouseout= function (){hoverOFF(9)};
+	board[10].onmouseout= function (){hoverOFF(10)};	
+	board[11].onmouseout= function (){hoverOFF(11)};
+
 
 
 	//board[i].onmouseover = function(){hoverON(i-3)};
 			//board[i].onmouseout = function(){hoverOFF(i-3)};
 
-	/*
+
+	function winner(){
+		//First Row
+		if (gamestate=[0] + gamestate[1] + gamestate[2]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[0] + gamestate[1] + gamestate[2]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Second Row
+		if (gamestate[3] + gamestate[4] + gamestate[5]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[3] + gamestate[4] + gamestate[5]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Third Row
+		if (gamestate[6] + gamestate[7] + gamestate[8]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[6] + gamestate[7] + gamestate[8]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+
+		//First Column
+		if (gamestate[0] + gamestate[3] + gamestate[6]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[0] + gamestate[3] + gamestate[6]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Second Column
+		if (gamestate[1] + gamestate[4] + gamestate[7]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[1] + gamestate[4] + gamestate[7]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Third Column
+		if (gamestate[2] + gamestate[5] + gamestate[8]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[2] + gamestate[5] + gamestate[8]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Left Diagonal
+		if (gamestate[0] + gamestate[4] + gamestate[8]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[0] + gamestate[4] + gamestate[8]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}
+
+		//Right Diagonal
+		if (gamestate[2] + gamestate[4] + gamestate[6]==3){
+			status.innerHTML="Player X is the winner!";
+			status.setAttribute("class","you-won");
+		} else if (gamestate[2] + gamestate[4] + gamestate[6]== -3){
+			status.innerHTML="Player O is the winner!";
+			status.setAttribute("class","you-won");
+		}else{
+			status.innerHTML="It's not over yet!";
+		}
+		
+	}
 	function hoverON(x){
 		board[x].classList.add("hover");
 	}
 
 	function hoverOFF(x){
 		board[x].classList.remove("hover");
-	} */
+	}
 
 
 }
