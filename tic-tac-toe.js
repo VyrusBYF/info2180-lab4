afe5fcbb6fc71ad22c370df4e20fcc16781e0771
Richard Howlett <richardhowlett14@gmail.com>
2019-10-21 21:40:38 -0400

    Progress on exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,21 +2,39 @@
 //Tic-Tac-Toe Gam
 
 window.onload = function() {
-
-	var seeboard = document.querySelectorAll("div");
-	//seeboard.setAttribute("class", "square");
-
-	for (var i=3; i < seeboard.length - 1; i++) {
-		seeboard[i].setAttribute("class", "square");
-	}
-	console.log(Object.keys(seeboard).length);
-
-	var gamestate = [];
-
-	//Ensures that object is an array
-	//console.log(Object.prototype.toString.call(gamestate)); 
-
+	document.getElementsByTagName("button")[0].onclick = seeBoard;
 	
+	var gamestate=[0,0,0,0,0,0,0,0,0]; //Place holders for grid state
+	var turncount = 0; //Keeps track of the amount of turns, cannot be greater than 9
+	var state=0;
+	var board = document.getElementsByTagName("div");
+	var status = document.getElementById("status");
+
+	//Applies the square class to all divs contained in the board section of the html code
+	function seeBoard(){
+		for (var i=3; i < board.length - 1; i++) {
+			board[i].setAttribute("class", "square");
+			board[i].innerHTML="";
+			board[i].setAttribute("onclick","makeMove("+i+")");
+		}
+	}
+
+	function makeMove(x){
+		if (board[x].className != "square X" && board[x].className != "square O"){
+			gamestate[x]=state;
+			option(x);
+		}
+
+
+	}
+
+	function option (){
+		if (turncount<9){
+			if (board[x].classList.contains("hover")){
+				
+			}
+		}
+	}
 
 
 }
